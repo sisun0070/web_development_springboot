13a5e949957bdff549e69364aa4698f386c7d2cb
sisun0070 <jisun5812@naver.com>
2024-12-24 21:50:23 +0900

    feat: chapter03

---
+++ hwangjisun_springboot/chapter03/src/main/java/me/hwangjisun/springbootdeveloper/MemberRepository.java
@@ -0,0 +1,8 @@
+package me.hwangjisun.springbootdeveloper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
---
+++ hwangjisun_springboot/chapter03/src/main/java/me/hwangjisun/springbootdeveloper/TestControllor.java
@@ -0,0 +1,22 @@
+package me.hwangjisun.springbootdeveloper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TestControllor {
+
+    @Autowired // TestService Bean 주입
+    TestService testService;
+
+    @GetMapping("/test")
+    public List<Member> getAllMembers() {
+        List<Member> members = testService.getAllMembers();
+        return members;
+    }
+
+}
+
