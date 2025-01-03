6602c7a1ece1c8cf0f6fa756d6d59af6ae2df4e2
sisun0070 <jisun5812@naver.com>
2025-01-03 21:45:24 +0900

    feat : 20250103

--- hwangjisun_springboot/chapter05/src/main/java/me/hwangjisun/springbootdeveloper/Member.java
+++ hwangjisun_springboot/chapter05/src/main/java/me/hwangjisun/springbootdeveloper/Member.java
@@ -16,10 +16,13 @@ public class Member {
 
     @Column(name = "name", nullable = false)
     private String name;    // DB 테이블의 'name' 컬럼과 매칭
+
+
     /*
         각 애너테이션의 역할은 lombok의 경우 java 과정에서 다 설명했습니다.
         하지만 추후 다시 설명드리겠습니다.
         현재는 member라는 이름의 테이블에 접근하는 데 사용하는 객체로 이해해주세요.
         이후 member 테이블과 Member 클래스를 매핑하는 코드를 작성할 예정입니다.
      */
+
 }
