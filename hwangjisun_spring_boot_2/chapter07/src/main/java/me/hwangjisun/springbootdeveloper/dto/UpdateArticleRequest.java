9da4fe86d9b99bd931a0833276c1fa0bab66628d
sisun0070 <jisun5812@naver.com>
2025-01-10 21:47:48 +0900

    feat : blogviewmaking

---
+++ hwangjisun_spring_boot_2/chapter07/src/main/java/me/hwangjisun/springbootdeveloper/dto/UpdateArticleRequest.java
@@ -0,0 +1,12 @@
+package me.hwangjisun.springbootdeveloper.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateArticleRequest {
+    private String title;
+    private String content;
+}
